cbb831106e5f313c7dc3fbc22a217cffe562af99
cank92 <cankaleli@gmail.com>
2025-01-13 15:45:48 +0300

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,33 +1,75 @@
-  <?php
-    $errors = '';
-    $myemail = 'info@uce.lv';//<-----Put Your email address here.
-    if(empty($_POST['name'])  ||
-       empty($_POST['email']) ||
-       empty($_POST['message']))
+<?php
+if (isset($_POST['email'])) {
+
+    // REPLACE THIS 2 LINES AS YOU DESIRE
+    $email_to = "you@awesomecompany.com";
+    $email_subject = "You've got a new submission";
+
+    function problem($error)
     {
-        $errors .= "\n Error: all fields are required";
+        echo "Oh looks like there is some problem with your form data: <br><br>";
+        echo $error . "<br><br>";
+        echo "Please fix those to proceed.<br><br>";
+        die();
     }
-    $name = $_POST['name'];
-    $email_address = $_POST['email'];
-    $message = $_POST['message'];
-    if (!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-    $email_address))
+
+    // validation expected data exists
+    if (
+        !isset($_POST['fullName']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])
+    ) {
+        problem('Oh looks like there is some problem with your form data.');
+    }
+
+    $name = $_POST['fullName']; // required
+    $email = $_POST['email']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'Email address does not seem valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'Name does not seem valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'Message should not be less than 2 characters<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details following:\n\n";
+
+    function clean_string($string)
     {
-        $errors .= "\n Error: Invalid email address";
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
     }
-    
-    if( empty($errors))
-    {
-    $to = $myemail;
-    $email_subject = "Contact form submission: $name";
-    $email_body = "You have received a new message. ".
-    " Here are the details:\n Name: $name \n ".
-    "Email: $email_address\n Message \n $message";
-    $headers = "From: $myemail\n";
-    $headers .= "Reply-To: $email_address";
-    mail($to,$email_subject,$email_body,$headers);
-    //redirect to the 'thank you' page
-    header('Location: contact-form-thank-you.html');
-    }
-    ?>
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- Replace this as your success message -->
+
+    Thanks for contacting us, we will get back to you as soon as possible.
+
+<?php
+}
+?>
